bd73044159350326cc4a6bd697786485c9a00950
Denuwan-md <denuwanmd@gmail.com>
2024-12-28 13:50:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/k1RH9bH/1086.jpg",
+ALIVE_MSG: process.env.ALVE_MSG || "කොහමද මම Denuwan md whatsapp bot මාව නිර්මාණ කරනු ලැබුවේ Denuwan විසිනි මගේ විදාන ලබා ගැනුම හදහා menu යන කමාන්ඩ් එක භාවිතා කරන්න",
 };
