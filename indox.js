e4474f4589f1c3b8d3cad4e5ebb947845ce618e7
Denuwan-md <denuwanmd@gmail.com>
2024-12-29 18:59:33 +0530

    Update indox.js

--- indox.js
+++ indox.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `Denuwan md bot connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.ibb.co/k1RH9bH/1086.jpg` }, caption: up })
 
 }
 })
