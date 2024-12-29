c70eb2f6deec8eca9066b3061d8704d081b5ec88
Denuwan-md <denuwanmd@gmail.com>
2024-12-29 15:02:36 +0530

    Update songvideo.js

--- plugins/songvideo.js
+++ plugins/songvideo.js
@@ -14,7 +14,33 @@ try{
 if(!q) return reply("url not inbut sorry")
 const search = await yts (q)
 const date = search.videos[0]:
+const url = date.url
+
+let desc = '
+🎧 DENUWAN MD SONG DOWNLOAD 🎧   
   
+title: ${data.title}
+description: ${data.description}
+time: ${deta.timestamp}
+ago: ${data.ago}  
+views: ${data.views} 
+
+MADE BY DENUWAN-MD🙂  
+'
+await conn.sendmessage(form,{image:{url: data.thumbnail},caption:desc},{quoted:mek}):
+
+    //download audio    
+
+let down = await fg.yta(url)
+let downloadurl = down.dl_url
+
+//send audio massage
+await conn.sendmassage(from,{audio: {url:downloadurl},mimeaudio"audio/mpeg"},{quoted:mak})
+
+
+
+
+
 }catsh(e){
 consoli.log(e)
 reply('${e}')
