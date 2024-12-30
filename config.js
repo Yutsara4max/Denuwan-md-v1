a27a401982ba17785da0fbb1d72b703c85790282
Denuwan-md <denuwanmd@gmail.com>
2024-12-30 07:46:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/k1RH9bH/1086.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hi.i am Denuwan md whatsapp bot, iam alive new",
 };
