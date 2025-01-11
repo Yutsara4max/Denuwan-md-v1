79ae6662cc21455566a489829a01757aa9aeb2c1
Denuwan-md <denuwanmd@gmail.com>
2025-01-11 08:21:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,4 @@ module.exports = {
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/k1RH9bH/1086.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hi.i am Denuwan md whatsapp bot, iam alive new",
 SESSION_ID: process.env.SESSION_ID || "nQATlZia#6dF-DwCL6B299GCvnk2gTZAj4tl3Db0iuziHmOIVSV0",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  
 };
